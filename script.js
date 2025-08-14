d038b995a7630b4b45ddcb8251c1b863efc501e0
Ronald Kelley <kelro.privet@gmail.com>
2025-08-13 19:53:46 -0500

    Initial commit for StarSite

---
+++ script.js
@@ -0,0 +1,9 @@
+function alertMessage() {
+  alert("Thanks for visiting StarSite! 🌟 We'll help you shine online.");
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  alert("Message sent! We'll get back to you soon. 📧");
+  return false;
+}
